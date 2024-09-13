7553bc53d3cf194c124e5f381668670e8e7bc9d4
Michael <michael.zusmanovskiy@gmail.com>
2024-09-13 18:42:05 +0200

    CLI

--- Comet.py
+++ Comet.py
@@ -8,19 +8,41 @@ def comet(
     filename :str,
     p: Annotated[bool, typer.Option(help="to print out a comet.params.new file")] = False,
     q: Annotated[bool, typer.Option(help="to print out a comet.params.new file with more parameter entries")] = False,
+    ap: Annotated[str, typer.Option(help="to specify an alternate parameters file (default comet.params)")] = "",
+    n: Annotated[str, typer.Option(help="to specify an alternate output base name; valid only with one input file")] = "",
     d: Annotated[str, typer.Option(help="to specify a sequence database, overriding entry in parameters file")] = "",
     f: Annotated[int, typer.Option(help="to specify the first/start scan to search, overriding entry in parameters file")] = None,
     l: Annotated[int, typer.Option(help="to specify the last/end scan to search, overriding entry in parameters file")] = None,
+    i: Annotated[str, typer.Option(help="create peptide index file only (specify .idx file as database for index search)")] = ""
     ):
-    #print("Usage: 'python Comet.py --help'")
     """
     Comet in Python
     """
+    if p:
+        pass
 
+    if q:
+        pass
 
-    #TODO
+    if ap != "":
+        pass
+
+    if n != "":
+        pass
+
+    if d != "":
+        pass
+
+    if f != None:
+        pass
+
+    if l != None:
+        pass
+
+    if i != "":
+        pass
+
     
-
 if __name__ == "__main__":
     app()
 
